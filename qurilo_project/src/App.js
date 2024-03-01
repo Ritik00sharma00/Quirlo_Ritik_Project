233b7d1c061f222da7b68262b713286118ba9bfa
Ritik00sharma00 <114102148+Ritik00sharma00@users.noreply.github.com>
2024-03-01 10:39:23 +0530

    Update App.js

--- qurilo_project/src/App.js
+++ qurilo_project/src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import Mern_stack from './Components/Mern_stack';
-import Mern_form from './Components/Mern_form';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
